6262ec2e1ba2a99c3d93b6d4a78d888f3ae7b9d3
Gowtham G <gowthamg.cs17@bitsathy.ac.in>
2020-09-29 13:02:31 +0530

    adding-changes

---
+++ mysql/mytable.sql
@@ -0,0 +1,6 @@
+CREATE table test( student_id char(10) PRIMARY KEY, name varchar(20), email varchar(30), department char(10), phone BIGINT);
+INSERT INTO test VALUES(1,"Kavinraja G","kavinrajag@coda.global","MTS",1215434321);
+INSERT INTO test VALUES(2,"Gautham G","gauti@gmail.com","CSE",1234567899);
+INSERT INTO test VALUES(3,"Hemachander R","hema@gmail.com","MTS",7324169557);
+INSERT INTO test VALUES(4,"Balaji U","balu@gmail.com","IT",7237693457);
+INSERT INTO test VALUES(5,"Govindhraj K","govindh@gmail.com","IT",9394665257);
